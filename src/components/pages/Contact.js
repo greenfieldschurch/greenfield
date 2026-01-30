9aa7dd8e0d0e8db9d7c8649bc83e3f6a7ea077ce
Deepak Thankachan <greenfieldevangelicalchurch@gmail.com>
2026-01-30 00:45:07 +0000

    fix bugs

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -28,37 +28,17 @@ function Contact() {
         </div>
 
         <div className="contact-body">
-          <div className="form-calendar-wrapper">
-            <div className="contact-form-section">
-              <div className="google-form-container">
-                <iframe 
-                  src="https://docs.google.com/forms/d/e/1FAIpQLSeaNl8ajMBEySsSXwSQmz0cL7ffOiZyelriosARWq33JY-FHA/viewform?embedded=true"
-                  className="google-form-iframe"
-                  frameBorder="0"
-                  marginHeight="0"
-                  marginWidth="0"
-                >
-                  Loading…
-                </iframe>
-              </div>
-            </div>
-
-            <div className='calendar-section'>
-              <div className='calendar-container'>
-                <div className='calendar-header'>
-                  <i className='far fa-calendar-alt calendar-icon'></i>
-                  <h2>Upcoming Events</h2>
-                  <p className='calendar-subtitle'>Join us for worship and fellowship</p>
-                </div>
-                <div className='calendar-wrapper'>
-                  <iframe 
-                    src="https://calendar.google.com/calendar/embed?src=greenfieldscommunitychurch%40gmail.com&ctz=Europe%2FLondon&showTitle=0&showNav=1&showDate=1&showPrint=0&showTabs=1&showCalendars=0&showTz=1&mode=AGENDA"
-                    className='calendar-iframe'
-                    frameBorder="0" 
-                    scrolling="no"
-                  ></iframe>
-                </div>
-              </div>
+          <div className="contact-form-section">
+            <div className="google-form-container">
+              <iframe 
+                src="https://docs.google.com/forms/d/e/1FAIpQLSeaNl8ajMBEySsSXwSQmz0cL7ffOiZyelriosARWq33JY-FHA/viewform?embedded=true"
+                className="google-form-iframe"
+                frameBorder="0"
+                marginHeight="0"
+                marginWidth="0"
+              >
+                Loading…
+              </iframe>
             </div>
           </div>
 
